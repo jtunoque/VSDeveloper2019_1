2da11623eaed77f111cc360c9125c0eeab9ead9b
javiertuga <javiertuga@gmail.com>
2019-02-21 22:24:25 -0500

    add cap 04

---
+++ Cap04/slnApp/App.EF.CodeFirst.Test/AristDATest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using App.EF.CodeFirst.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.EF.CodeFirst.Test
+{
+    [TestClass]
+    public class AristDATest
+    {
+        [TestMethod]
+        public void Count()
+        {
+            var da = new ArtistDA();
+            Assert.IsTrue(da.Count() > 0);
+        }
+        [TestMethod]
+        public void Insert()
+        {
+            var da = new ArtistDA();
+            var id = da.Insert(
+                new Artist() {Name="Artits_" 
+                            + Guid.NewGuid().ToString() }
+                );
+
+            Assert.IsTrue(id > 0);
+
+        }
+
+        [TestMethod]
+        public void DeleteBatch()
+        {
+            var list = new List<int>();
+            list.Add(1283);
+            list.Add(1282);
+            list.Add(1281);
+            var da = new ArtistDA();
+            Assert.IsTrue(da.DeleteBatch(list));
+        }
+    }
+}
---
+++ Cap04/slnApp/App.EF.CodeFirst/ArtistDA.cs
@@ -0,0 +1,84 @@
+﻿using App.EF.CodeFirst.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.EF.CodeFirst
+{
+    public class ArtistDA
+    {
+        private ChinookModel _context;
+
+        public ArtistDA()
+        {
+            _context = new ChinookModel();
+        }
+
+        public int Count()
+        {
+            return _context.Artist.Count();
+        }
+
+        public IEnumerable<Artist> Gets(string filterByName)
+        {
+            return _context.Artist.Where(item => item.Name.Contains(filterByName)).ToList();
+        }
+        public Artist Get(int id)
+        {
+            return _context.Artist.Find(id);
+        }
+
+        public int Insert(Artist entity)
+        {
+            //Se agrega la entidad al contexto de Entity Framework
+            _context.Artist.Add(entity);
+
+            //Se confirma la transaccion
+            _context.SaveChanges();
+
+            return entity.ArtistId;
+
+        }
+
+        public bool Update(Artist entity)
+        {
+            //Se atacha la entidad al contexto de Entity Framework
+            _context.Artist.Attach(entity);
+            _context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+
+           //Se confirma la transaccion
+           var result = _context.SaveChanges();
+
+           return result>0;
+
+        }
+
+        public bool Delete(Artist entity)
+        {
+            _context.Artist.Attach(entity);
+            _context.Artist.Remove(entity);
+
+            //Se confirma la transaccion
+            var result = _context.SaveChanges();
+
+            return result > 0;
+        }
+
+        public bool DeleteBatch(List<int> ids)
+        {
+            foreach (var i in ids)
+            {
+                var entity = new Artist() { ArtistId = i };
+                _context.Artist.Attach(entity);
+                _context.Artist.Remove(entity);
+            }
+            //Se confirma la transaccion
+            var result = _context.SaveChanges();
+
+            return result > 0;
+        }
+
+    }
+}
