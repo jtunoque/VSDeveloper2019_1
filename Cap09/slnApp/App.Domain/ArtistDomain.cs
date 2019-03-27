65b8cd8fe8164e8c9248a45548d82e3573f0db31
javiertuga <javiertuga@gmail.com>
2019-03-26 22:25:21 -0500

    add cap 9

---
+++ Cap09/slnApp/App.Domain/ArtistDomain.cs
@@ -0,0 +1,41 @@
+﻿using App.DataAccess.Repository;
+using App.Domain.Interfaces;
+using App.Entities.Base;
+using App.Entities.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain
+{
+    public class ArtistDomain : IArtistDomain
+    {
+        public IEnumerable<Artist> GetArtists(string nombre)
+        {
+            IEnumerable<Artist> result = new List<Artist>();
+
+            using(var uw = new AppUnitOfWork())
+            {
+                result = uw.ArtistRepository.GetAll(
+                            item=>item.Name.Contains(nombre)
+                    );
+            }
+
+            return result;
+        }
+
+        public IEnumerable<TrackAll> GetTracksAll(string nombre)
+        {
+            IEnumerable<TrackAll> result = new List<TrackAll>();
+
+            using (var uw = new AppUnitOfWork())
+            {
+                result = uw.TrackRepository.GetTracksAll(nombre);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Cap09/slnApp/App.Services.WCF/Reportes/Implements/ReporteTracks.cs
@@ -0,0 +1,21 @@
+﻿using App.Domain;
+using App.Domain.Interfaces;
+using App.Entities.Queries;
+using App.Services.WCF.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.WCF
+{
+    public partial class ReporteServices : IReporteServices
+    {
+        public IEnumerable<TrackAll> GetTrackAll(string trackName)
+        {
+            ITrackDomain domain = new TrackDomain();
+            return domain.GetTracksAll(trackName);
+        }
+    }
+}
